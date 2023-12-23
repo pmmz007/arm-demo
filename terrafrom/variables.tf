340f98e61bf6c9523e51d9de2ec1e6a4e62f01e5
Johnie Zaw <phonemyintmyatzaw77@gmail.com>
2023-12-23 11:37:18 +0630

    added cred

---
+++ terrafrom/variables.tf
@@ -0,0 +1,18 @@
+variable "client_id" {
+    type = string
+    description = "(optional) describe your variable"
+}
+
+variable "client_secret" {
+    type = string
+    description = "(optional) describe your variable"
+}
+
+variable "tenant_id" {
+    type = string
+}    description = "(optional) describe your variable"
+
+variable "subscription_id" {
+    type = string
+    description = "(optional) describe your variable"
+}
